1a74b306743852466bdbceacf0be3853bf66d1a2
Tomd304 <tomd304@live.co.uk>
2021-12-15 16:24:56 +0000

    Added event listeners to buttons & underline to button current module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import {createHomepage, createMenu, createContact} from './homepage';
+import {createHomepage} from './homepage';
+import {createMenu} from './menu';
+import {createContact} from './contact';
 
-createContact();
+let homeButton = document.querySelector('#homeBtn')
+let menuButton = document.querySelector('#menuBtn')
+let contactButton = document.querySelector('#contactBtn')
+
+createHomepage();
+homeButton.style.textDecoration = 'underline'
+
+homeButton.addEventListener('click', () => {
+    createHomepage()
+    homeButton.style.textDecoration = 'underline'
+    menuButton.style.textDecoration = ''
+    contactButton.style.textDecoration = ''
+});
+
+menuButton.addEventListener('click', () => {
+    createMenu()
+    homeButton.style.textDecoration = ''
+    menuButton.style.textDecoration = 'underline'
+    contactButton.style.textDecoration = ''
+});
+
+contactButton.addEventListener('click', () => {
+    createContact()
+    homeButton.style.textDecoration = ''
+    menuButton.style.textDecoration = ''
+    contactButton.style.textDecoration = 'underline'
+});
+
